b9bf9e4193ec358662ee9234db0ac2a59ca76c19
Jack LaVelle <jacklavelle17@gmail.com>
2023-06-17 23:23:13 -0400

    run CreateReport.py and start from there ... issues with extending multiple Paragraphs at once

--- CreateReport.py
+++ CreateReport.py
@@ -1,6 +1,7 @@
 from reportlab.lib.pagesizes import letter
 from reportlab.lib.styles import getSampleStyleSheet, ParagraphStyle
-from reportlab.platypus import SimpleDocTemplate, Paragraph
+from reportlab.platypus import SimpleDocTemplate, Paragraph, Image
+from Section import Section
 
 
 def generate_pdf():
@@ -11,6 +12,7 @@ def generate_pdf():
     # Define paragraph styles
     styles = getSampleStyleSheet()
     bold_style = styles["Title"]
+    bold_style.alignment = 0
     italic_underline_style = ParagraphStyle(
         "italic_underline",
         parent=styles["Normal"],
@@ -23,14 +25,57 @@ def generate_pdf():
 
     # Define the content
     content = []
-    content.append(Paragraph("<b>OWM Nutrition Genome</b>", bold_style))
-    content.append(Paragraph("<u><i>Brain Health</i></u>", italic_underline_style))
+    content.append(Paragraph("Nutrition Genome", bold_style))
+    content.extend([createSection(), createSubSection()])
     content.append(Paragraph(get_lorem_ipsum(), regular_style))
+    
+    # Add the image at the top-center of the first page
+    image_path = "logo.png"  # Replace with the path to your PNG file
+    image = Image(image_path)
+    image.drawHeight = 100 # Adjust the image height as needed
+    image.drawWidth = 100   # Scale the image while maintaining aspect ratio
+    image.hAlign = "CENTER"  # Center-align the image
+    image.spaceAfter = 20  # Add space after the image
+    content.insert(0, image)
 
     # Build the PDF document
     doc.build(content)
     print("PDF generated successfully!")
 
+def createSection():
+    styles = getSampleStyleSheet()
+    section_title_style = styles["SectionTitle"]
+    sectionHeader = Paragraph("<u><i>Brain Health</i></u>", section_title_style)
+    
+    return sectionHeader
+
+def createSubSection():
+    styles = getSampleStyleSheet()
+    significance_style = styles["Significance"]
+    significance = Paragraph("Lower cannabinoid levels: Higher anxiety and stress perception. Less 'blissful'", significance_style)
+    lifestyle_list = [
+          "Running and biking > 30 minutes",
+          "Strenuous hiking at high altitude",
+          "Meditation, Yoga and deep breathing"
+    ]
+    nutrition_list = [
+          "Red clover tea (women)",
+          "Kaempfero: green leafy vegetables, including spinach and kale, and herbs such as dill, chives, and tarragon",
+          "Cacao",
+          "Genistein: Soy milk, soy flour, soy protein isolates, textured soy protein, Tempe",
+          "Echinacea tea",
+          "7-hydroxyflavone (parsley, onions, berries, tea, and citrus fruits)",
+          "Beta-caryophyllene (cloves, rosemary, hops)"
+    ]
+    avoid_list = ["Pesticides", "CBD oil", "Phthalates"]
+    
+    lifestyle = Paragraph(lifestyle_list[0], styles["Bullet"])
+    subSection = []
+    subSection.extend([significance, lifestyle])
+    
+    return subSection
+    
+    
 
 def get_lorem_ipsum():
     # Replace this with your own paragraph or desired text
