6f5ddc3995ccc1a3e6b13aac4bc2aa444af97573
Jack LaVelle <jacklavelle17@gmail.com>
2023-08-07 21:43:45 -0400

    Updated tests to work with new key functionality

--- tests/ClientServerTests.py
+++ tests/ClientServerTests.py
@@ -12,26 +12,30 @@ from Utilities import MyEncoder
 
 
 class Tests(unittest.TestCase):
-    def test_upload_patients(self):
-        patients = {}
+    def test_upload_data(self):
+        data = {}
         # TODO: store patients by uuid
         names = ["George Washington", "Abraham Lincoln", "Gandalf"]
         for name in names:
-            patients[name] = Person(name)
+            data[name] = Person(name)
 
+        data["key"] = "dyqIDK3amOB09U4PSmSDW5FaZiFMNyoCTlmQESTBzh8="
         response = requests.post(
             "http://127.0.0.1:5000/save",
-            json=json.dumps(patients, cls=MyEncoder),
+            json=json.dumps(data, cls=MyEncoder),
             timeout=30,
         )
 
         self.assertEqual(response.status_code, 200)
 
-    def test_download_patients(self):
-        response = requests.get("http://127.0.0.1:5000/read")
-        patients = json.loads(response.json()["data"])
+    def test_download_data(self):
+        data = {"key": "dyqIDK3amOB09U4PSmSDW5FaZiFMNyoCTlmQESTBzh8="}
+        response = requests.get(
+            "http://127.0.0.1:5000/read", json=json.dumps(data), timeout=30
+        )
+        data = json.loads(response.json()["data"])
 
-        self.assertEqual(type(patients), dict)
+        self.assertEqual(type(data), dict)
 
 
 if __name__ == "__main__":
