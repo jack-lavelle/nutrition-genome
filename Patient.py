76b19b583cf29dccd7079e29f626c4e76c9d37b4
Jack LaVelle <jacklavelle17@gmail.com>
2023-11-22 23:12:12 -0500

    somewhat ironed out Patient class

--- Patient.py
+++ Patient.py
@@ -2,27 +2,24 @@ import PatientUtilities
 
 
 class Patient:
-    # TODO: finish this docstring
     """Contains information for a patient.
 
     Attributes:
         name (`str`): Name of the patient.
-        reportsDict (`dict`): Meant to serve as a way to retrieve previous reports, where keys are
-            the date of creation and values are the path to the report.
-        genes (`dict`):
+
+        objectives (`list`): List of the three goals the patient currently has.
+
+        category_genes_dict (`dict`): Contains all the genes a patient has and groups them by
+            their category.
     """
 
-    name = None
-    category_genes_dict = {}
-    objectives = {}
-
-    def __init__(
-        self, name=None, genes=PatientUtilities.generate_genes(), objectives=None
-    ) -> None:
+    def __init__(self, name, category_genes_dict, objectives) -> None:
         self.name = name
         self.objectives = objectives
-        self.category_genes_dict = genes
 
-    def create_report(self):
-        # https://github.com/python-openxml/python-docx
-        pass
+        if not category_genes_dict:
+            # TODO: Log this rather than print.
+            print("No genes given.")
+            category_genes_dict = PatientUtilities.generate_genes()
+
+        self.category_genes_dict = category_genes_dict
